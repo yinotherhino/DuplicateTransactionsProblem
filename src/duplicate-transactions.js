165161ce2aca06f1dbf1d609d43504970d2b1e66
yinotherhino <muhammedbayero@gmail.com>
2022-10-01 15:05:10 +0100

    partially completed duplicates

--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -7,7 +7,11 @@ function findDuplicateTransactions(transactions) {
     }
     let resultObj= {}
     let inputObj= JSON.parse(JSON.stringify(transactions))
-
+    inputObj.sort((a,b) =>{
+        a= new Date(a.time).getTime();
+        b= new Date(b.time).getTime();
+        return a - b;
+    })
     let multipleTrans = inputObj.map((x,i)=>{
         return inputObj[i]['sourceAccount']+ inputObj[i]['targetAccount']+ inputObj[i]['amount'] + inputObj[i]['targetAccount']+ inputObj[i]['category'];
     });
@@ -22,9 +26,15 @@ function findDuplicateTransactions(transactions) {
 
     inputObj.forEach((x,i)=>{
         let concat= inputObj[i]['sourceAccount']+ inputObj[i]['targetAccount']+ inputObj[i]['amount'] + inputObj[i]['targetAccount']+ inputObj[i]['category'];
-        if (resultObj[concat]){
-            resultObj[concat].push(inputObj[i]);
+        let timeOfTransaction= new Date (inputObj[i]['time']).getTime();
+        if(resultObj[concat] && resultObj[concat].length==0){
+            resultObj[concat].push(inputObj[i])
         }
+        else if(resultObj[concat] && resultObj[concat].length>0){ 
+            let timeOfLastTransaction= new Date (resultObj[concat][resultObj[concat].length-1]['time']);
+            if (resultObj[concat] && (timeOfTransaction - timeOfLastTransaction)<= 60000){
+            resultObj[concat].push(inputObj[i]);
+        }}
     })
 
     //duplicates have same source, target, category and amount
@@ -32,9 +42,13 @@ function findDuplicateTransactions(transactions) {
     // first group elements having same source and target and amount together
 
     //group these elements by 1min diffs
-
+    let arrays= Object.keys(resultObj);
+    let result=[];
+    for (let i=0; i<arrays.length; i++){
+        result.push(resultObj[arrays[i]])
+    }
     
-    return resultObj
+    return result
 
 }
 
