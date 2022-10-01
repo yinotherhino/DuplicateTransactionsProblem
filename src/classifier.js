bb0de2b59e1d0d000b747efc0a4e2efd4dcc7ba8
yinotherhino <muhammedbayero@gmail.com>
2022-10-01 11:50:07 +0100

    classifier partially completed

--- src/classifier.js
+++ src/classifier.js
@@ -1,42 +1,65 @@
 function classifier(input) {
-    if (input=[]){
-        return {'noOfGroups': 0}
+    if(!Array.isArray(input)){
+        throw Error
     }
-    else if(!Array.isArray(input)){
-        throw "Input should be an array"
+    else if (Array.isArray(input) && !input.length){
+        return {'noOfGroups': 0}
     }
     //copy input into another object so that the initial object is preserved
     let inputObj= JSON.parse(JSON.stringify(input))
-    //use a count variable to name the groups when a group is formed count ++ and group name = 'group'+count
-
-    let agesArray= inputObj.map((x) => Number(x.dob.slice(0,10)));
-    agesArray= agesArray.sort((a,b)=> Number(a.slice(0,4) + a.slice(5,7) + a.slice(8,10)) -Number(b.slice(0,4)  + b.slice(5,7) + b.slice(8,10)));
+    let baseDate= new Date(2019, 0, 1);
+    let agesArray= inputObj.map((x)=> {
+        // if(x.regNo[0]=='0'){
+        //     regNos= x.regNo.slice(1)
+        // }
+        
+        return [x.name, x.regNo, Math.ceil((baseDate-new Date(x.dob))/(1000*60*60*24*365).toFixed(1)), x.dob,]
+        
+    })
+    
+    agesArray= agesArray.sort((a,b)=> a[2] -b[2]);
     // console.log(agesArray)
-    let count=1;
-    let obj= {};
+    // return agesArray;
+    let count=0;
+    let obj= {
+    };
     for (let i=0; i<agesArray.length; i++){
         // TO DO I have to convert the agesArray to Concatenated Numbers for easy comparison
-        if (!obj['group'+count]){
-            obj['group'+count]=[agesArray[i]]
+        if (!obj['group'+count] || obj['group'+count]['members'].length==3){
+            count++
+            obj.noOfGroups = count;
+            // obj['group'+count]=[agesArray[i]]
+            obj['group'+count]= {
+                members: [
+                    {name:agesArray[i][0], dob: agesArray[i][3], regNo: agesArray[i][1],age:agesArray[i][2]}
+                ],
+                oldest: agesArray[i][2],
+                sum: agesArray[i][2],
+                regNos: [Number(agesArray[i][1])]
+            }
         }
-        else if((agesArray[i]-obj['group'+count][0])<=50000){
-            obj['group'+count].push(agesArray[i]);
+        //if age diff is less than 5 and members are not yet up to three add new member
+        else if((agesArray[i][2] - obj['group'+count]['members'][obj['group'+count]['members'].length-1]['age'])<=5 && obj['group'+count]['members'].length<=3){
+            obj['group'+count]['members'].push({name:agesArray[i][0], dob: agesArray[i][3], regNo: agesArray[i][1], age:agesArray[i][2] });
+            obj['group'+count]['oldest']= agesArray[i][2];
+            obj['group'+count]['sum']= obj['group'+count]['sum'] + agesArray[i][2];
+            obj['group'+count]['regNos'].push(Number(agesArray[i][1]))
+            obj['group'+count]['regNos'].sort((a,b)=> a-b)
         }
-        else if((agesArray[i]-obj['group'+count][0])>50000){
+        else if((agesArray[i][2] - obj['group'+count]['members'])>5 || obj['group'+count]['members'].length<=3){
             count++;
-            obj['group'+count]=[agesArray[i]]
-
+            obj.noOfGroups = count;
+            obj['group'+count]= {
+                members: [
+                    {name:agesArray[i][0], dob: agesArray[i][3], regNo: agesArray[i][1], age: agesArray[i][2] }
+                ],
+                oldest: agesArray[i][2],
+                sum: agesArray[i][2],
+                regNos: [Number(agesArray[i][1])]
+            }
         }
     }
-    //input is an array of objects that will be accessed with a for loop
-
-    // dob= slice the dob property at index 9 to cut out the time
-
-
-    //sort by age and group into categories including ages in the same 5 year range
-
-    //
-
+    return(obj);
 }
 
 
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,6 +1,20 @@
 function findDuplicateTransactions(transactions) {
+    if(!Array.isArray(transactions)){
+        throw Error
+    }
+    else if (Array.isArray(transactions) && transactions.length<2){
+        return [];
+    }
+
+    //duplicates have same source, target, category and amount
+    //but different time and id
+    // first group elements having same source and target and amount together
+
+    //group these elements by 1min diffs
 
     
+    return transactions
+
 }
 
 export default findDuplicateTransactions;
