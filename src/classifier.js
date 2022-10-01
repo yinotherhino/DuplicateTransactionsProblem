6591fc0a8fa16df7a89e5fb988b4b8e51bd4f27c
yinotherhino <muhammedbayero@gmail.com>
2022-10-01 21:27:00 +0100

    passed all tests

--- src/classifier.js
+++ src/classifier.js
@@ -1,62 +1,65 @@
-function classifier(input) {
-    //for invalid inputs throw error
-    if(!Array.isArray(input)){
-        throw Error
+function findDuplicateTransactions(transactions) {
+    if(!Array.isArray(transactions)){
+        throw Error;
     }
-    //for empty array return {noOfGroups: 0}
-    else if (Array.isArray(input) && !input.length){
-        return {'noOfGroups': 0}
+    else if (Array.isArray(transactions) && transactions.length<2){
+        return [];
     }
-    //copy input into another object so that the initial object is not mutated
-    let inputObj= JSON.parse(JSON.stringify(input));
-    //assume current date is 2019
-    let baseDate= new Date(2019, 1, 1).getFullYear();
-    let groupNo=0;
-    let result= {};
-    //an array of the details we need from the input to enable easy navigation
-    let arrayOfDetails= inputObj.map((x)=> {
-        return [x.name, x.regNo, baseDate- (new Date(x.dob)).getFullYear(), x.dob]
+    let resultObj= {}
+    let inputObj= JSON.parse(JSON.stringify(transactions))
+    inputObj.sort((a,b) =>{
+        a= new Date(a.time).getTime();
+        b= new Date(b.time).getTime();
+        return a - b;
     })
-    //sort arrayOfDetails by age which is at the second index of each element
-    arrayOfDetails= arrayOfDetails.sort((a,b)=> a[2] -b[2]);
+    let multipleTrans = inputObj.map((x,i)=>{
+        return inputObj[i]['sourceAccount']+ inputObj[i]['targetAccount']+ inputObj[i]['amount'] + inputObj[i]['targetAccount']+ inputObj[i]['category'];
+    });
+    multipleTrans= multipleTrans.filter((x,i)=>{
+        return (multipleTrans.slice(0,i).includes(multipleTrans[i]) || multipleTrans.slice(i+1).includes(multipleTrans[i]))
+    })
 
-    for (let i=0; i<arrayOfDetails.length; i++){
-        // create new group if group doesnt exist yet or group length is already equals 3
-        if (!result['group'+groupNo] || result['group'+groupNo]['members'].length==3){
-            groupNo++
-            result.noOfGroups = groupNo;
-            result['group'+groupNo]= {
-                members: [
-                    {name:arrayOfDetails[i][0], dob: arrayOfDetails[i][3], regNo: arrayOfDetails[i][1],age:arrayOfDetails[i][2]}
-                ],
-                oldest: arrayOfDetails[i][2],
-                sum: arrayOfDetails[i][2],
-                regNos: [Number(arrayOfDetails[i][1])]
-            }
+    let duplicateTransactions= Array.from(new Set(multipleTrans))
+    duplicateTransactions.forEach((x) => {
+        resultObj[x]=[]
+    })
+
+    inputObj.forEach((x,i)=>{
+        let concat= inputObj[i]['sourceAccount']+ inputObj[i]['targetAccount']+ inputObj[i]['amount'] + inputObj[i]['targetAccount']+ inputObj[i]['category'];
+        // let timeOfTransaction= new Date (inputObj[i]['time']).getTime();
+        if(!resultObj[concat]){
+         return
         }
-        //if age diff is less than 5 and members are not yet up to three add new member
-        else if((arrayOfDetails[i][2] - result['group'+groupNo]['members'][result['group'+groupNo]['members'].length-1]['age'])<=5 && result['group'+groupNo]['members'].length<=3){
-            result['group'+groupNo]['members'].push({name:arrayOfDetails[i][0], dob: arrayOfDetails[i][3], regNo: arrayOfDetails[i][1], age:arrayOfDetails[i][2] });
-            result['group'+groupNo]['oldest']= arrayOfDetails[i][2];
-            result['group'+groupNo]['sum']= result['group'+groupNo]['sum'] + arrayOfDetails[i][2];
-            result['group'+groupNo]['regNos'].push(Number(arrayOfDetails[i][1]))
-            result['group'+groupNo]['regNos'].sort((a,b)=> a-b)
+        // else if(resultObj[concat] && resultObj[concat].length==0){
+        else if(resultObj[concat] ){ 
+            resultObj[concat].push(inputObj[i])
         }
-        //if age difference between last member and new prospective member is more than 5 create new group
-        else if((arrayOfDetails[i][2] - result['group'+groupNo]['members'])>5 || result['group'+groupNo]['members'].length<=3){
-            groupNo++;
-            result.noOfGroups = groupNo;
-            result['group'+groupNo]= {
-                members: [
-                    {name:arrayOfDetails[i][0], dob: arrayOfDetails[i][3], regNo: arrayOfDetails[i][1], age: arrayOfDetails[i][2] }
-                ],
-                oldest: arrayOfDetails[i][2],
-                sum: arrayOfDetails[i][2],
-                regNos: [Number(arrayOfDetails[i][1])]
+        // else if(resultObj[concat] && resultObj[concat].length>0 ){ 
+        //     let timeOfLastTransaction= new Date (resultObj[concat][resultObj[concat].length-1]['time']).getTime();
+        //     if (resultObj[concat] && (timeOfTransaction - timeOfLastTransaction)<= 60000){
+        //     resultObj[concat].push(inputObj[i]);
+        // }}
+    })
+
+    let arrays= Object.keys(resultObj);
+    let result=[];
+    for (let i=0; i<arrays.length; i++){
+        result.push(resultObj[arrays[i]])
+    }
+    for (let i=0; i<result.length; i++){
+        for (let j=1; j<result[i].length; j++){
+            let timeOfLastTransaction= new Date (result[i][j-1]['time']).getTime();
+            let timeOfTransaction= new Date (result[i][j]['time']).getTime();
+            if((timeOfTransaction- timeOfLastTransaction)>60000){
+                result.push(result[i].slice(j));
+                result[i]= result[i].slice(0,j)
             }
         }
     }
-    return(result);
+    result= result.filter(x=> x.length>1)
+    
+    return result
+
 }
 
 
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,6 +1,6 @@
 function findDuplicateTransactions(transactions) {
     if(!Array.isArray(transactions)){
-        throw Error
+        throw Error;
     }
     else if (Array.isArray(transactions) && transactions.length<2){
         return [];
@@ -26,29 +26,32 @@ function findDuplicateTransactions(transactions) {
 
     inputObj.forEach((x,i)=>{
         let concat= inputObj[i]['sourceAccount']+ inputObj[i]['targetAccount']+ inputObj[i]['amount'] + inputObj[i]['targetAccount']+ inputObj[i]['category'];
-        let timeOfTransaction= new Date (inputObj[i]['time']).getTime();
-        if(resultObj[concat] && resultObj[concat].length==0){
+        if(!resultObj[concat]){
+         return
+        }
+        else if(resultObj[concat] ){ 
             resultObj[concat].push(inputObj[i])
         }
-        else if(resultObj[concat] && resultObj[concat].length>0){ 
-            let timeOfLastTransaction= new Date (resultObj[concat][resultObj[concat].length-1]['time']);
-            if (resultObj[concat] && (timeOfTransaction - timeOfLastTransaction)<= 60000){
-            resultObj[concat].push(inputObj[i]);
-        }}
     })
 
-    //duplicates have same source, target, category and amount
-    //but different time and id
-    // first group elements having same source and target and amount together
-
-    //group these elements by 1min diffs
     let arrays= Object.keys(resultObj);
     let result=[];
     for (let i=0; i<arrays.length; i++){
         result.push(resultObj[arrays[i]])
     }
+    for (let i=0; i<result.length; i++){
+        for (let j=1; j<result[i].length; j++){
+            let timeOfLastTransaction= new Date (result[i][j-1]['time']).getTime();
+            let timeOfTransaction= new Date (result[i][j]['time']).getTime();
+            if((timeOfTransaction- timeOfLastTransaction)>60000){
+                result.push(result[i].slice(j));
+                result[i]= result[i].slice(0,j)
+            }
+        }
+    }
+    result= result.filter(x=> x.length>1)
     
-    return result
+    return result.sort((a,b)=> a[0]['id']-b[0]['id'])
 
 }
 
