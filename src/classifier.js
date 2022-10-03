95593e9b77290c37a7854671aec1eaa51bc9929b
yinotherhino <muhammedbayero@gmail.com>
2022-10-03 16:02:12 +0100

    used objects in arrayofdetails

--- src/classifier.js
+++ src/classifier.js
@@ -15,36 +15,31 @@ function classifier(input) {
     let result= {};
     //an array of the details we need from the input to enable easy navigation
     let arrayOfDetails= inputObj.map((x)=> {
-        return [x.name, x.regNo, baseDate- (new Date(x.dob)).getFullYear(), x.dob]
+        return {'name':x.name, 'regNo': x.regNo, 'age': baseDate- (new Date(x.dob)).getFullYear(), 'dob':x.dob};
     })
-    //sort arrayOfDetails by age which is at the second index of each element
-    arrayOfDetails= arrayOfDetails.sort((a,b)=> a[2] -b[2]);
-
+    //sort arrayOfDetails by age
+    arrayOfDetails= arrayOfDetails.sort((a,b)=> a.age -b.age);
     for (let i=0; i<arrayOfDetails.length; i++){
-        // create new group if group doesnt exist yet or group length is already equals 3
-        if (!result['group'+groupNo] || result['group'+groupNo]['members'].length==3 || (arrayOfDetails[i][2] - result['group'+groupNo]['members'][0]['age'])>5){
-            groupNo++
+        // create new group if group doesnt exist yet or group length is already equals 3 or age is 5yrs older than youngest member
+        if (!result['group'+groupNo] || result['group'+groupNo]['members'].length==3 || (arrayOfDetails[i].age - result['group'+groupNo]['members'][0]['age'])>5){
+            groupNo++;
             result.noOfGroups = groupNo;
             result['group'+groupNo]= {
-                members: [
-                    {name:arrayOfDetails[i][0], dob: arrayOfDetails[i][3], regNo: arrayOfDetails[i][1],age:arrayOfDetails[i][2]}
-                ],
-                oldest: arrayOfDetails[i][2],
-                sum: arrayOfDetails[i][2],
-                regNos: [Number(arrayOfDetails[i][1])]
+                members: [arrayOfDetails[i]],
+                oldest: arrayOfDetails[i].age,
+                sum: arrayOfDetails[i].age,
+                regNos: [Number(arrayOfDetails[i].regNo)]
             }
         }
         //if age diff is less than 5 and members are not yet up to three add new member
-        else if((arrayOfDetails[i][2] - result['group'+groupNo]['members'][result['group'+groupNo]['members'].length-1]['age'])<=5 && result['group'+groupNo]['members'].length<=3){
-            result['group'+groupNo]['members'].push({name:arrayOfDetails[i][0], dob: arrayOfDetails[i][3], regNo: arrayOfDetails[i][1], age:arrayOfDetails[i][2] });
-            result['group'+groupNo]['oldest']= arrayOfDetails[i][2];
-            result['group'+groupNo]['sum']= result['group'+groupNo]['sum'] + arrayOfDetails[i][2];
-            result['group'+groupNo]['regNos'].push(Number(arrayOfDetails[i][1]))
-            result['group'+groupNo]['regNos'].sort((a,b)=> a-b)
+        else if((arrayOfDetails[i].age - result['group'+groupNo]['members'][0]['age'])<=5 && result['group'+groupNo]['members'].length<3){
+            result['group'+groupNo]['members'].push(arrayOfDetails[i]);
+            result['group'+groupNo]['oldest']= arrayOfDetails[i].age;
+            result['group'+groupNo]['sum']= result['group'+groupNo]['sum'] + arrayOfDetails[i].age;
+            result['group'+groupNo]['regNos'].push(Number(arrayOfDetails[i].regNo));
+            result['group'+groupNo]['regNos'].sort((a,b)=> a-b);
         }
     }
     return(result);
 }
-
-
 export default classifier;
